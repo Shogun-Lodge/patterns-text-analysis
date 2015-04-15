f97b1ba3b8265f1a2b61121b1b1ac86fed0019b5
Shogun Lodge <shogunlodgeservices@gmail.com>
2015-04-15 22:35:38 +1000

    More comments/Cleared old junk/ASCII total

--- Tweet_to_OSC_TCP_JSON.ino
+++ Tweet_to_OSC_TCP_JSON.ino
@@ -7,9 +7,15 @@
   
   Marcus Cook
   
-  13/04/2015
+  Shogun Lodge Services
+  
+  15/04/2015
+  
+  !!! Work in progress - Much serial printing / debugging !!!
 
-  EtherMega hence 24AA025E48 MAC Chip code (I2C stuff)
+  EtherMega hence 24AA025E48 MAC Chip code (I2C stuff) - Currently removed
+  
+  All spelling mistakes are intentional and quite witty
 
 */
 
@@ -19,8 +25,8 @@
 #include <OSCBundle.h>
 #include <ArduinoJson.h>
 
-#define I2C_ADDRESS 0x50  // THese are for the 24AA025E48 MAC Chip
-#include <Wire.h>
+//#define I2C_ADDRESS 0x50  // These are for the 24AA025E48 MAC Chip
+//#include <Wire.h>
 
 EthernetUDP Udp;
 
@@ -28,10 +34,12 @@ EthernetUDP Udp;
 IPAddress ip(192, 168, 10, 15);
 //destination IP
 IPAddress outIp(192, 168, 10, 11);
+//port to send OSC out on
 const unsigned int outPort = 9000;
 
 byte mac[] = { 0x24, 0xAA, 0xBE, 0xEF, 0xFE, 0xED }; // you can find this written on the board of some Arduino Ethernets or shields
 
+//variables of text to be sent via OSC
 int txtLngth = 0;
 int numbers = 0;
 int letters = 0;
@@ -40,26 +48,22 @@ int lower = 0;
 int upper = 0;
 int punct = 0;
 int others = 0;
+int ascii = 0;
 
+//variables needed for text analysis 
 char tcpIn = 'A';
 String dataBuf = "";
-//String userOSC = "";
-//String idOSC = "";
-//char userOSC[20] = "";
-//char idOSC[20] = "";
 const char* user = "";
 const char* id = "";
 char json[300] = "";
-
-//const char* user[] = {""};
-//const char* id[] = {""};
-
-int oscFlag = 0;
-int serialFlag = 0;
-int tcpFlag = 0;
 int textAly = 0;
 int feels = 0;
 
+//flags need for tcp and OSC operation
+int oscFlag = 0;
+int tcpFlag = 0;
+
+//blank OSC varaiables (to be removed later)
 float para1 = 0;
 float para2 = 0;
 float para3 = 0;
@@ -70,18 +74,22 @@ float para7 = 0;
 float para8 = 0;
 float para9 = 0;
 
+//Start TCP Server (listen on port 8000)
 EthernetServer server = EthernetServer(8000);
 
-void setup() {
-  Wire.begin();
-  mac[0] = readRegister(0xFA);
-  mac[1] = readRegister(0xFB);
-  mac[2] = readRegister(0xFC);
-  mac[3] = readRegister(0xFD);
-  mac[4] = readRegister(0xFE);
-  mac[5] = readRegister(0xFF);
-  Ethernet.begin(mac,ip);
-  Udp.begin(8888);
+void setup(){
+  delay(250);                              // In to try and get EtherTen to send OSC - didn't work
+  //Extarct MAC address via I2C
+  //Wire.begin();
+  //mac[0] = readRegister(0xFA);
+  //mac[1] = readRegister(0xFB);
+  //mac[2] = readRegister(0xFC);
+  //mac[3] = readRegister(0xFD);
+  //mac[4] = readRegister(0xFE);
+  //mac[5] = readRegister(0xFF);
+  
+  Ethernet.begin(mac,ip);                  //Set MAC and local IP
+  Udp.begin(8888);                         //Start UPD listening on port 8888
   
   Serial.begin(57600);
   
@@ -92,36 +100,21 @@ void setup() {
 
 void loop(){
   // if an incoming client connects, there will be bytes available to read:
-  //Serial.println("Waiting...");
   EthernetClient client = server.available();
-  if (client == true) {
+  if (client == true){
     tcpFlag = 1;
     oscFlag = 0;
     tcpIn = client.read();
-    //dataBuf += tcpIn;
-    //Serial.print("feels = ");
-    //Serial.println(feels);
     json[feels] = tcpIn;
     feels++;
-    //Serial.println(x);  
   } 
- // else if (client != true && flag == 1){
- //   Serial.println(data);
- //   data = "";  
- //   tcpFlag = 0;
- // }  
   
     while (client != true && tcpFlag == 1){
-  //while (Serial.available() > 0) { // add any incoming characters to the String:
-    //int textAly = Serial.read();
-    //json = dataBuf;
     feels = 0;
     Serial.println("--------");
     Serial.println("");
     Serial.println("TCP OUT!");
-    Serial.println("");
     Serial.println(dataBuf);
-   // Serial.println(json);
     StaticJsonBuffer<300> jsonBuffer;
     JsonObject& root = jsonBuffer.parseObject(json);
     
@@ -141,52 +134,68 @@ void loop(){
     Serial.print("id = ");
     Serial.println(id);
     
-    dataBuf = text;
-    //userOSC = root["user"];
-    //idOSC = root["id"];
+    dataBuf = text;                    // Move text into String buffer
     
     int len = dataBuf.length();
-    txtLngth = len - 12;
+    txtLngth = len - 12;               // Text length minus '@twitterbot '
+    
+      //Cycyle though text String buffer analysing each char minus '@twitterbot '
       for(int i = 12; i < len; i++){
       textAly = dataBuf.charAt(i);
-    
-        //txtLngth++;  
-        if(isDigit(textAly)) {
-        numbers++;
+      ascii = ascii + textAly;        // Total ASCII value of text
+        if(isDigit(textAly)) {        // # of numbers in text
+        numbers++;                    
         }
-        if(isAlpha(textAly)) {
-          letters++;
+        if(isAlpha(textAly)) {        // # of letters in text
+          letters++;                 
         }
-        if(isSpace(textAly)) {
-          spaces++;
+        if(isSpace(textAly)) {        // # of spaces in text
+          spaces++;                  
         }
-        if(isLowerCase(textAly)) {
+        if(isLowerCase(textAly)) {    // # of lower case letters in text
           lower++;
         }
-        if(isUpperCase(textAly)) {
+        if(isUpperCase(textAly)) {    // # of upper case letters in text
           upper++;
         }
-        if(isPunct(textAly)) {
+        if(isPunct(textAly)) {        // # of 'puncuation' in text
           punct++;
         }
-        else{
+        else{                        // # of 'others' in text
           others++;
         }
       }
-   // serialFlag = 1;
-   tcpFlag = 0;
-   oscFlag = 1;
-   textAly = 0;
-   dataBuf = "";
+
+   tcpFlag = 0;                      // Clear TCP flag
+   oscFlag = 1;                      // Set OSC flag
+   textAly = 0;                      // Claera text analyse varaiable
+   dataBuf = "";                     // Clear text String buffer
+  
   Serial.println("");
   Serial.println("TEXT ANALYSED");
   Serial.println("");
+  Serial.print("Text Length = ");
+  Serial.println(txtLngth);
   Serial.print("Letters = ");
   Serial.println(letters);
+  Serial.print("ASCII = ");
+  Serial.println(ascii);
+  Serial.print("Upper = ");
+  Serial.println(upper);
+  Serial.print("Lower = ");
+  Serial.println(lower);
+  Serial.print("Spaces = ");
+  Serial.println(spaces);
+  Serial.print("Numbers = ");
+  Serial.println(numbers);
+  Serial.print("Punctuation = ");
+  Serial.println(punct);
+  Serial.print("'Others' = ");
+  Serial.println(others);
   Serial.println("");
   }
  
-   if(oscFlag == 1){
+   if(oscFlag == 1){               // Are we ready for OSC!
      
      para1 = txtLngth;
      para2 = numbers;
@@ -199,6 +208,7 @@ void loop(){
    
      OSCBundle bndl;
    
+      //Build OSC bundle
       bndl.add("/para1").add(para1);
       bndl.add("/para2").add(para2);
       bndl.add("/para3").add(para3);
@@ -207,14 +217,17 @@ void loop(){
       bndl.add("/para6").add(para6);
       bndl.add("/para7").add(para7);
       bndl.add("/para8").add(para8);
+      bndl.add("/para9").add(ascii);
       bndl.add("/user").add(user);
       bndl.add("/id").add(id);
       
-      Udp.beginPacket(outIp, outPort);
-          bndl.send(Udp); // send the bytes to the SLIP stream
-      Udp.endPacket(); // mark the end of the OSC Packet
-      bndl.empty(); // empty the bundle to free room for a new one
+      //Send OSC bundle out to the world
+      Udp.beginPacket(outIp, outPort);    // IP of destiantion and port
+          bndl.send(Udp);                 // send the bytes to the SLIP stream
+      Udp.endPacket();                    // mark the end of the OSC Packet
+      bndl.empty();                       // empty the bundle to free room for a new one
       
+      //Clear all text avariables
       txtLngth = 0;
       numbers = 0;
       letters = 0;
@@ -223,16 +236,17 @@ void loop(){
       upper = 0;
       punct = 0;
       others = 0;
+      ascii = 0;
      
-      oscFlag = 0;
+      oscFlag = 0;                      // Clear OSC flag
+      
       Serial.println("OSC SENT FLAGS SET");
       Serial.println("");
       Serial.println("--------");
       Serial.println("");
-    //  serialFlag = 0;
    }    
 }
-
+/*
  // MAC address from 24AA025E48 MAC Chip via I2C
 byte readRegister(byte r){
   unsigned char v;
@@ -247,3 +261,4 @@ byte readRegister(byte r){
   v = Wire.read();
   return v;
 }
+*/
